45c7d68bc3989dbcdefd513915df97479ae53ed4
Rebecca Mutek <mutek.b@gmail.com>
2021-12-21 21:53:42 -0600

    added abstract virtual class to Employee class

--- oop.cpp
+++ oop.cpp
@@ -1,54 +1,74 @@
 #include <iostream>
 using std::string;
 
-class Employee {
-    private:
-        string Name;
-        string Company;
-        string Email;
-        int Age;
+class AbstractEmployee {
+    virtual void AskForPromotion()=0;
+};
 
-    public:
-        void setName(string name) {
-            Name = name;
-        }
-        string getName() {
-            return Name;
-        }
+class Employee:AbstractEmployee {
+private:
+    string Name;
+    string Company;
+    string Email;
+    int Age;
+    int PerformanceRating;
 
-        void setCompany(string company) {
-            Company = company;
-        }
-        string getCompany() {
-            return Company;
-        }
-
-        void setAge(int age) {
-            if (age >= 18) {
-                Age = age;
-            }
-        }
-        int getAge() {
-            return Age;
-        }
-        Employee(string name, string company, int age) {
-            Name = name;
-            Company = company;
+public:
+    void setName(string name) {
+        Name = name;
+    }
+    string getName() {
+        return Name;
+    }
+    void setCompany(string company) {
+        Company = company;
+    }
+    string getCompany() {
+        return Company;
+    }
+    void setAge(int age) {
+        if (age >= 18) {
             Age = age;
         }
-        void IntroduceYourSelf() {
-            std::cout << "Hello, I am " << Name << std::endl;
-            std::cout << "I work at " << Company << std::endl;
-            std::cout << "I am " << Age << " Years old" << std::endl;
-        };
+    }
+    int getAge() {
+        return Age;
+    }
+    Employee(string name, string company, int age, int rating) {
+        Name = name;
+        Company = company;
+        Age = age;
+        PerformanceRating = rating;
+    }
+    void IntroduceYourSelf() {
+        std::cout << "Hello, I am " << Name << std::endl;
+        std::cout << "I work at " << Company << std::endl;
+        std::cout << "I am " << Age << " Years old" << std::endl;
+    };
+    void AskForPromotion() {
+        if (PerformanceRating >= 90) {
+            std::cout << Name << " was promoted!!" << std::endl;
+        } else if (PerformanceRating >= 80) {
+            std::cout << Name << " you will be re-evaluated for a promotion in 3 months" << std::endl;
+        } else {
+            std::cout << Name << " sorry no promotion for you" << std::endl;
+        }
+    };
+
 };
 
 int main()
 {
-    Employee employee1 = Employee("Rebecca", "Google", 30);
+    Employee employee1 = Employee("Rebecca", "Google", 30, 94);
     employee1.IntroduceYourSelf();
+    employee1.AskForPromotion();
 
-    Employee employee2 = Employee("John", "Google", 34);
+    Employee employee2 = Employee("John", "Google", 34, 80);
     employee2.IntroduceYourSelf();
+    employee2.AskForPromotion();
+
+    Employee employee3 = Employee("Sal", "Pizza Pi Co.", 32, 76);
+    employee3.IntroduceYourSelf();
+    employee3.AskForPromotion();
     return 0;
 }
